a6c81851815f2cf835bd1c82a37330b294cb8a82
Florent FERRY <florent@ouvrages-web.fr>
2017-06-22 14:26:57 +0200

    Fix issue with invalid block has wrong index

--- lib/ouvrages_block/block_field.rb
+++ lib/ouvrages_block/block_field.rb
@@ -3,8 +3,15 @@ module BlockField
     @template.render partial: "admin/blocks/blocks_form", locals: { form: self }
   end
 
-  def block_form(block)
-    index = block.new_record? ? "__NEW__#{block.class.name.underscore.singularize.upcase}__" : nil
+  def block_form(block, options = {})
+    template = options.delete(:template)
+
+    if template
+      index = "__NEW__#{block.class.name.underscore.singularize.upcase}__"
+    else
+      index = nil
+    end
+
     @template.render partial: "admin/blocks/block_form", locals: { form: self, block: block, index: index }
   end
 end
