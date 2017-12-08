94c2b2a84990ef7a06be2cfbe22132e31752ce25
Florent FERRY <florent@ouvrages-web.fr>
2017-12-08 11:06:03 +0100

    Convert to slim

--- lib/generators/block/block_generator.rb
+++ lib/generators/block/block_generator.rb
@@ -31,8 +31,8 @@ class BlockGenerator < Rails::Generators::NamedBase
   end
 
   def create_view_files
-    template "views/admin.html.haml", File.join("app", "views", "admin", standard_block_name_plural, "_block_form.html.haml")
-    template "views/show.html.haml", File.join("app", "views", standard_block_name_plural, "_#{standard_block_name_singular}.html.haml")
+    template "views/admin.html.slim", File.join("app", "views", "admin", standard_block_name_plural, "_block_form.html.slim")
+    template "views/show.html.slim", File.join("app", "views", standard_block_name_plural, "_#{standard_block_name_singular}.html.slim")
   end
 
   private
